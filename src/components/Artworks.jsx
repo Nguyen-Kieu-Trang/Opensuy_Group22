ae8971454e02b2fff5edaff1578e155e8cd35586
Nguyen Kieu Trang <trang.nk205174@sis.hust.edu.vn>
2023-05-27 15:51:45 +0700

    image

---
+++ src/components/Artworks.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import { setGlobalState, useGlobalState } from '../store'
+
+const Artworks = () => {
+  const [nfts] = useGlobalState('nfts')
+  const [end, setEnd] = useState(4)
+  const [count] = useState(4)
+  const [collection, setCollection] = useState([])
+
+  const getCollection = () => {
+    return nfts.slice(0, end)
+  }
+
+  useEffect(() => {
+    setCollection(getCollection())
+  }, [nfts, end])
+
+  return (
+    <div className="bg-[#151c25] gradient-bg-artworks">
+      <div className="w-4/5 py-10 mx-auto">
+        <h4 className="text-white text-3xl font-bold uppercase text-gradient">
+          {collection.length > 0 ? 'Latest Artworks' : 'No Artworks Yet'}
+        </h4>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-4 lg:gap-3 py-2.5">
+          {collection.map((nft, i) => (
+            <Card key={i} nft={nft} />
+          ))}
+        </div>
+
+        {collection.length > 0 && nfts.length > collection.length ? (
+          <div className="text-center my-5">
+            <button
+              className="shadow-xl shadow-black text-white
+            bg-[#e32970] hover:bg-[#bd255f]
+            rounded-full cursor-pointer p-2"
+              onClick={() => setEnd(end + count)}
+            >
+              Load More
+            </button>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  )
+}
+
+
+export default Artworks
