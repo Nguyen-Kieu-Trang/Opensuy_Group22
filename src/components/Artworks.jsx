d0e4a79644787c0fd98ce02a38783adcef013a55
Nguyen Kieu Trang <trang.nk205174@sis.hust.edu.vn>
2023-05-28 10:03:35 +0700

    update1

--- src/components/Artworks.jsx
+++ src/components/Artworks.jsx
@@ -45,5 +45,37 @@ const Artworks = () => {
   )
 }
 
+const Card = ({ nft }) => {
+  const setNFT = () => {
+    setGlobalState('nft', nft)
+    setGlobalState('showModal', 'scale-100')
+  }
+
+  return (
+    <div className="w-full shadow-xl shadow-black rounded-md overflow-hidden bg-gray-800 my-2 p-3">
+      <img
+        src={nft.metadataURI}
+        alt={nft.title}
+        className="h-60 w-full object-cover shadow-lg shadow-black rounded-lg mb-3"
+      />
+      <h4 className="text-white font-semibold">{nft.title}</h4>
+      <p className="text-gray-400 text-xs my-1">{nft.description}</p>
+      <div className="flex justify-between items-center mt-3 text-white">
+        <div className="flex flex-col">
+          <small className="text-xs">Current Price</small>
+          <p className="text-sm font-semibold">{nft.cost} ETH</p>
+        </div>
+
+        <button
+          className="shadow-lg shadow-black text-white text-sm bg-[#e32970]
+            hover:bg-[#bd255f] cursor-pointer rounded-full px-1.5 py-1"
+          onClick={setNFT}
+        >
+          View Details
+        </button>
+      </div>
+    </div>
+  )
+}
 
 export default Artworks
