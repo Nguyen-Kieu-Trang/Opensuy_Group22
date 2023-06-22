b1b2f740e44b32c69f661fc858f0fb7d752618e3
Nguyen Kieu Trang <kieutrang260702@gmail.com>
2023-06-22 18:36:43 +0700

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
 
 import { useEffect } from 'react'
+import { getAllNFTs, isWallectConnected } from './Blockchain.Services'
+import Alert from './components/Alert'
+import Artworks from './components/Artworks'
+import CreateNFT from './components/CreateNFT'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Loading from './components/Loading'
+import ShowNFT from './components/ShowNFT'
+import Transactions from './components/Transactions'
+import UpdateNFT from './components/UpdateNFT'
+
+const App = () => {
+  useEffect(async () => {
+    await isWallectConnected()
+    await getAllNFTs()
+  }, [])
+
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-hero">
+        <Header />
+        <Hero />
+      </div>
+      <Artworks />
+      <Transactions />
+      <CreateNFT />
+      <ShowNFT />
+      <UpdateNFT />
+      <Alert />
+      <Loading />
+    </div>
+  )
+}
 
 export default App;
 
